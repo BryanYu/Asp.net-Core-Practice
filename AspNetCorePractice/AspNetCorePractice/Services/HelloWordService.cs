62370b898d0693bbcb261878ae16442364310434
BryanYu <ak78912@hotmail.com>
2019-03-17 22:04:44 +0800

    #01-Startup

---
+++ AspNetCorePractice/AspNetCorePractice/Services/HelloWordService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Startup.Services
+{
+    public class HelloWordService
+    {
+        public string HelloWord()
+        {
+            return "Hello Word From " + nameof(HelloWordService);
+        }
+    }
+}
