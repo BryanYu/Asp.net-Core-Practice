988084f9b2580e4d7fe447cf12f2f7fd12f8cbdc
BryanYu <d58526@gmail.com>
2019-08-06 15:55:56 +0800

    Asp.net Core 03

---
+++ AspNetCorePractice/03-Middleware/Middleware/Controllers/TestController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Middleware.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        // GET api/test
+        [HttpGet]
+        [Route("Map1")]
+        public ActionResult<string> Get()
+        {
+            return "value1";
+        }
+
+        // GET api/test/5
+        [HttpGet("{id}")]
+        [Route(("Map2"))]
+        public ActionResult<string> Get(int id)
+        {
+            return "value2";
+        }
+
+       
+    }
+}
---
+++ AspNetCorePractice/03-Middleware/Middleware/Startup.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Middleware
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            #region app.Run
+            //// app.Run 會中止Request Pipeline
+            //app.Run(async context =>
+            //{
+            //    await context.Response.WriteAsync("Hello From App.Run MiddleWare");
+            //});
+
+            ////下面的BeforeMiddleware01不會執行
+            //app.Use(
+            //    async (context, next) =>
+            //    {
+            //        Console.WriteLine("BeforeMiddleware01");
+            //        await next.Invoke();
+            //        Console.WriteLine("AfterMiddleware01");
+            //    });
+
+            #endregion
+
+
+            #region app.Use
+            //app.Use 會將要求傳至下一個Middleware
+            // 以下設定呼叫的順序會是 BeforeMiddleware01 -> BeforeMiddleware02 ->
+            //                      AfterMiddleware02 -> AfterMiddleware01
+            //app.Use(
+            //    async (context, next) =>
+            //    {
+            //        Console.WriteLine("BeforeMiddleware01");
+            //        await next.Invoke();
+            //        Console.WriteLine("AfterMiddleware01");
+            //    });
+
+            //app.Use(
+            //    async (context, next) =>
+            //    {
+            //        Console.WriteLine("BeforeMiddleware02");
+            //        await next.Invoke();
+            //        Console.WriteLine("AfterMiddleware02");
+            //    });
+
+            #endregion
+
+            #region app.Map
+            // app.Map 是app.Run的擴充，可以根據route路徑來套用app.Run的MiddleWare 
+
+            app.Map("/map1",
+                config =>
+                {
+                    config.Run(async (context) =>
+                    {
+                        await context.Response.WriteAsync("Map Test 1");
+                    });
+                });
+
+
+            app.Map("/map2",
+                config =>
+                {
+                    config.Run(async (context) =>
+                    {
+                        await context.Response.WriteAsync("Map Test 2");
+                    }); 
+                });
+
+            #endregion
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+
+            
+           
+        }
+    }
+}
